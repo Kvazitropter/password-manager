12764d9555b8964d68dfc5185ee095ac60625faa
Anna Kotova <kotovanna2001@gmail.com>
2025-04-25 10:01:32 +0300

    Connected db. Closes #5

---
+++ backend/connection.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+
+def __get_connection():
+    con = psycopg2.connect(
+        port = 5432,
+        host = 'localhost',
+        database = 'password_manager',
+        user = 'postgres',
+        password = 'xxXX1234'
+    )
+    
+    return con
+
+def get_accounts():
+    con = __get_connection()
+    cur = con.cursor()
+    cur.execute('SELECT * FROM account_details')
+    return cur.fetchall()

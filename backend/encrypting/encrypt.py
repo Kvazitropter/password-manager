dad1380ddb14eab33dd728d076d7ddffc2588367
Anna Kotova <kotovanna2001@gmail.com>
2025-04-29 18:42:12 +0300

    fix encrypting

--- backend/encrypting/encrypt.py
+++ backend/encrypting/encrypt.py
@@ -1,12 +1,13 @@
 import base64
 import os
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
-def encrypting_mk(master_key, salt=''):
-    if salt == '':
+def encrypt(master_key, value_to_encrypt='control', salt=None):
+    if not salt:
         salt = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -16,5 +17,5 @@ def encrypting_mk(master_key, salt=''):
     )
     key = base64.urlsafe_b64encode(kdf.derive(master_key.encode()))
     f = Fernet(key)
-    encrypted_control_string = f.encrypt(b"control")
-    return encrypted_control_string, salt
+    encrypted_value = f.encrypt(value_to_encrypt.encode())
+    return encrypted_value, salt
--- backend/queries/mk_funcs.py
+++ backend/queries/mk_funcs.py
@@ -1,28 +1,38 @@
 import psycopg2
+
 from backend import connection
-from backend.encrypting.encrypting_mk import encrypting_mk
+from backend.encrypting.decrypt import decrypt
+from backend.encrypting.encrypt import encrypt
+
 
 def is_mk(mk):
     con = connection.__get_connection()
     cur = con.cursor()
-    cur.execute('SELECT encrypted_control_string, salt FROM master_key WHERE id = 1') #это в случае если у нас одна запись в мастеркей, иначе нужно будет брать во внимание айдишник 
-    db_mk, db_salt = cur.fetchone()
-    if (db_mk, db_salt):
-        entered_mk_encr, _ = encrypting_mk(mk, db_salt)
+    cur.execute('''SELECT encrypted_control_string, salt 
+                FROM master_key WHERE id = 1''')  
+    # потом будет (...WHERE login = %s),(login,) 
+    # и логин пердается в is_mk что ли
+    encrypted_value, salt = cur.fetchone()
+    encrypted_value = encrypted_value.tobytes()
+    salt = salt.tobytes()
+    try:
+        decrypt(mk, encrypted_value, salt)
+    except Exception:
+        return False
+    finally:
         cur.close()
         con.close()
-        return db_mk == entered_mk_encr
-    cur.close()
-    con.close()
-    return False
+    return True
 
     
 def add_new_mk(mk):
     con = connection.__get_connection()
     cur = con.cursor()
-    encrypted_control_string, salt = encrypting_mk(mk)
+    encrypted_control_string, salt = encrypt(mk)
     try:
-        cur.execute('INSERT INTO master_key (encrypted_control_string, salt) VALUES (%s, %s)', (encrypted_control_string, salt))
+        cur.execute('''INSERT INTO master_key 
+                    (encrypted_control_string, salt)
+                    VALUES (%s, %s)''', (encrypted_control_string, salt))
         con.commit()
         return True
     except psycopg2.Error:

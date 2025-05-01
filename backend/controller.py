9f7ee836855544ced88a8bdf327d08a1bcf5abc8
Kachulina Tatyana <kachulinatanya@yandex.ru>
2025-05-01 15:55:42 +0300

    add functions for getting, adding and deleting entries

--- backend/controller.py
+++ backend/controller.py
@@ -1,19 +1,22 @@
-from backend.encrypting.encrypt import encrypt
+from backend.custom_errors import (
+    ExistingLogin,
+    IncorrectMasterKey,
+    NonexistingLogin,
+)
 from backend.encrypting.decrypt import decrypt
-from backend.custom_errors import IncorrectMasterKey, NonexistingLogin, ExistingLogin
+from backend.encrypting.encrypt import encrypt
 
 
 class Controller():
     def __init__(self, repository):
         self.repository = repository    
 
-
     def is_existing_login(self, login):
         return self.repository.has_login_query(login)
 
     def login(self, login, entered_master_key):
         self.is_existing_login(login)
-        
+
         is_correct_login = self.is_existing_login(login)
 
         if not is_correct_login:
@@ -27,12 +30,25 @@ class Controller():
         if control_string != 'control':
             raise IncorrectMasterKey()
 
-
     def create_new_account(self, login, master_key):
         is_existing_login = self.is_existing_login(login)
-        
+
         if is_existing_login:
             raise ExistingLogin()
-        
+
         encrypted_master_key, salt = encrypt(master_key)
         self.repository.add_new_account_query(login, encrypted_master_key, salt)
+        
+    def get_entries(self, login):
+        return self.repository.get_all_entries_query(login)
+
+    def add_new_entry(
+        self, user_login, master_key, service_name, login, password
+    ):
+        encrypted_password, salt = encrypt(master_key, password)
+        self.repository.add_new_entry_query(
+            user_login, service_name, login, encrypted_password, salt
+        )
+
+    def delete_entry(self, entry_id):
+        self.repository.delete_entry_query(entry_id)
--- backend/repository.py
+++ backend/repository.py
@@ -2,7 +2,7 @@ import psycopg2
 
 
 class Repository:
-    def get_connection(self):
+    def __get_connection(self):
         connection = psycopg2.connect(
             port=5432,
             host='localhost',
@@ -10,51 +10,86 @@ class Repository:
             user='postgres',
             password='xxXX1234'
         )
-        
+
         return connection
-    
-    
-    def close_connection(self, connection, cur):
+
+    def __close_connection(self, connection, cur):
         connection.close()
         cur.close()
 
-
     def has_login_query(self, login):
-        con = self.get_connection()
+        con = self.__get_connection()
         cur = con.cursor()
-        
+
         cur.execute(
-            'SELECT EXISTS(SELECT 1 FROM master_key WHERE login = (%s))',
+            'SELECT EXISTS(SELECT 1 FROM user_account WHERE login = (%s))',
             (login,)
         )
         result = cur.fetchone()[0]
 
-        self.close_connection(con, cur)
+        self.__close_connection(con, cur)
         return result
 
-
     def get_memory_mk_and_salt_query(self, login):
-        con = self.get_connection()
+        con = self.__get_connection()
         cur = con.cursor()
 
         cur.execute(
-            'SELECT salt, encrypted_control_string FROM master_key WHERE login = %s',
+            '''SELECT (salt, encrypted_control_string) 
+FROM user_account WHERE login = %s''',
             (login,)
         )
         result = cur.fetchone()
 
-        self.close_connection(con, cur)
+        self.__close_connection(con, cur)
         return result
 
-
     def add_new_account_query(self, login, encrypted_control_string, salt):
-        con = self.get_connection()
+        con = self.__get_connection()
         cur = con.cursor()
 
         cur.execute(
-            'INSERT INTO master_key (login, encrypted_control_string, salt) VALUES (%s, %s, %s)',
+            '''INSERT INTO user_account (login, encrypted_control_string, salt) 
+VALUES (%s, %s, %s)''',
             (login, encrypted_control_string, salt)
         )
         con.commit()
 
-        self.close_connection(con, cur)
+        self.__close_connection(con, cur)
+
+    def get_all_entries_query(self, user_login):
+        con = self.__get_connection()
+        cur = con.cursor()
+
+        cur.execute(
+            'SELECT * FROM account_entries WHERE user_login = %s',
+            (user_login)
+        )
+        result = cur.fetchall()
+
+        self.__close_connection(con, cur)
+        return result
+
+    def add_new_entry_query(
+        self, user_login, service_name, login, encrypted_password, salt
+    ):
+        con = self.__get_connection()
+        cur = con.cursor()
+
+        cur.execute(
+            '''INSERT INTO account_entries (user_login, service_name, login, 
+encrypted_password, salt) VALUES (%s, %s, %s, %s)''',
+            (user_login, service_name, login, encrypted_password, salt)
+        )
+        con.commit()
+
+        self.__close_connection(con, cur)
+
+    def delete_entry_query(self, entry_id):
+        con = self.__get_connection()
+        cur = con.cursor()
+
+        cur.execute('DELETE FROM account_entries WHERE id = %s', (entry_id))
+        con.commit()
+
+        self.__close_connection(con, cur)
